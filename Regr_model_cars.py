4f2d4f5101ef27f8cbd558ce9b6c174d4694957c
Jammijuix <etumnukelvin@yahoo.com>
2026-06-01 11:15:15 +0100

    pic

--- Regr_model_cars.py
+++ Regr_model_cars.py
@@ -27,7 +27,7 @@ final_fi = load_feature_importance("feature_importance.xlsx")
 #sidebar setup
 Base_dir = os.path.dirname(__file__)
 
-image_sidebar = Image.open(os.path.join(Base_dir, "regLinearModel_-car_predictions/pic1.png"))
+image_sidebar = Image.open(os.path.join(Base_dir, "pic1.PNG"))
 st.sidebar.image(image_sidebar, width="stretch")
 st.sidebar.header("Vehicle Features")
 
@@ -64,7 +64,7 @@ def get_user_input():
     return user_data
 
 #top banner
-image_banner = Image.open(os.path.join(Base_dir, "regLinearModel_-car_predictions/pic2.png"))
+image_banner = Image.open(os.path.join(Base_dir, "pic2.PNG"))
 st.image(image_banner, width="stretch")
 
 #centre title
